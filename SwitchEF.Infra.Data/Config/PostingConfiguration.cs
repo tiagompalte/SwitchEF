f36ef540aedf8087ac16e835459d524f42ddd6d0
Tiago M Palte <tiagompalte@hotmail.com>
2019-01-08 20:44:25 -0200

    Relacionamento um para muitos invertido

---
+++ SwitchEF.Infra.Data/Config/PostingConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SwitchEF.Domain.Entities;
+
+namespace SwitchEF.Infra.Data.Config
+{
+    public class PostingConfiguration : IEntityTypeConfiguration<Posting>
+    {
+        public void Configure(EntityTypeBuilder<Posting> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Date).IsRequired();
+            builder.Property(p => p.Text).IsRequired().HasMaxLength(400);
+            builder.HasOne(p => p.User).WithMany(u => u.Postings);
+        }
+    }
+}
